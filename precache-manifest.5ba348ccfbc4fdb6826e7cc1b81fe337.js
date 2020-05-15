df303583754e205839b92aac59f98deee2aafd1b
nebojsaj1726 <nebojsa.jacovic@gmail.com>
2020-05-15 18:27:57 +0200

    Updates

---
+++ precache-manifest.5ba348ccfbc4fdb6826e7cc1b81fe337.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91369ab0c821eb20dfc5aa553e68c7c",
+    "url": "/magemonkeys/index.html"
+  },
+  {
+    "revision": "38807d09e805973c2eb9",
+    "url": "/magemonkeys/static/css/main.eba9704a.chunk.css"
+  },
+  {
+    "revision": "14c33e7c07f9732db785",
+    "url": "/magemonkeys/static/js/2.03f294c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/magemonkeys/static/js/2.03f294c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38807d09e805973c2eb9",
+    "url": "/magemonkeys/static/js/main.e59f24a7.chunk.js"
+  },
+  {
+    "revision": "106768b314f1560607ce",
+    "url": "/magemonkeys/static/js/runtime-main.53aa2f02.js"
+  },
+  {
+    "revision": "23054112eaab3c9a98cf3ac75e2ae514",
+    "url": "/magemonkeys/static/media/Avenir Heavy.23054112.woff"
+  },
+  {
+    "revision": "676ea0900d54a975584a3d0a584a49b9",
+    "url": "/magemonkeys/static/media/Avenir Heavy.676ea090.ttf"
+  },
+  {
+    "revision": "9be3d4e5ade9cfdf4d767cc9950664a0",
+    "url": "/magemonkeys/static/media/Avenir Heavy.9be3d4e5.eot"
+  },
+  {
+    "revision": "a700d36120c058e0b4fb12d784b37b29",
+    "url": "/magemonkeys/static/media/Avenir Heavy.a700d361.svg"
+  },
+  {
+    "revision": "251fd99dca4a15a63832304a46447f3c",
+    "url": "/magemonkeys/static/media/AvenirLTStd-Black.251fd99d.ttf"
+  },
+  {
+    "revision": "4fbd2bf6049e1f316ba29fe31b470d67",
+    "url": "/magemonkeys/static/media/AvenirLTStd-Black.4fbd2bf6.svg"
+  },
+  {
+    "revision": "7bd577037b33420747bb417d4afdc752",
+    "url": "/magemonkeys/static/media/AvenirLTStd-Black.7bd57703.eot"
+  },
+  {
+    "revision": "f403289bddcf0000d6ffcc7889a02e65",
+    "url": "/magemonkeys/static/media/AvenirLTStd-Black.f403289b.woff"
+  },
+  {
+    "revision": "0d7fbc4cc43a12f3fea29c8b55ed1a62",
+    "url": "/magemonkeys/static/media/FuturaStd-Heavy.0d7fbc4c.svg"
+  },
+  {
+    "revision": "164c9069636ec91d2d7a66d778c4ee35",
+    "url": "/magemonkeys/static/media/FuturaStd-Heavy.164c9069.ttf"
+  },
+  {
+    "revision": "8f017737dbddd288f24ac576e0ba3d38",
+    "url": "/magemonkeys/static/media/FuturaStd-Heavy.8f017737.woff"
+  },
+  {
+    "revision": "c2d8e42b6f3c6ff8366fd1efee26c36d",
+    "url": "/magemonkeys/static/media/FuturaStd-Heavy.c2d8e42b.eot"
+  },
+  {
+    "revision": "46f19fa767c903b61046b3cc0f9cc29b",
+    "url": "/magemonkeys/static/media/PlayfairDisplay-Regular.46f19fa7.eot"
+  },
+  {
+    "revision": "9bbf9c3f7c8e3dd7ac0935798b6e18b0",
+    "url": "/magemonkeys/static/media/PlayfairDisplay-Regular.9bbf9c3f.woff"
+  },
+  {
+    "revision": "b3721ba3bde34e5b38b0e1523cccfd7f",
+    "url": "/magemonkeys/static/media/PlayfairDisplay-Regular.b3721ba3.ttf"
+  },
+  {
+    "revision": "0b78ec9d509f67bfe3f8458c9d285df0",
+    "url": "/magemonkeys/static/media/avenirltstd-light.0b78ec9d.svg"
+  },
+  {
+    "revision": "61bd88af15d8a8bb16483d8b6b95d934",
+    "url": "/magemonkeys/static/media/avenirltstd-light.61bd88af.ttf"
+  },
+  {
+    "revision": "f3cf70ab639ab1d8d95c1ff668f0c16b",
+    "url": "/magemonkeys/static/media/avenirltstd-light.f3cf70ab.woff"
+  },
+  {
+    "revision": "f49140386fc019f11613ae5c4a4d5d1f",
+    "url": "/magemonkeys/static/media/avenirltstd-light.f4914038.eot"
+  },
+  {
+    "revision": "b902397e8c10109a42d5d08dcbbaf3cc",
+    "url": "/magemonkeys/static/media/b1.b902397e.png"
+  },
+  {
+    "revision": "a2249cb13528165f8efbaf55fa13f0e3",
+    "url": "/magemonkeys/static/media/b2b-monkey.a2249cb1.png"
+  },
+  {
+    "revision": "fe75524f5f3c868172d55fdc54b2f0c2",
+    "url": "/magemonkeys/static/media/black-back.fe75524f.jpg"
+  },
+  {
+    "revision": "7dece4467e1e9894f858aa224cd2d15b",
+    "url": "/magemonkeys/static/media/black-back2.7dece446.jpg"
+  },
+  {
+    "revision": "9eb642404e561daca388d8fe81e03ec6",
+    "url": "/magemonkeys/static/media/contact-monkey.9eb64240.png"
+  },
+  {
+    "revision": "39bd3a9a2adc657ebe9994ffbf80cef9",
+    "url": "/magemonkeys/static/media/home-banner.39bd3a9a.jpg"
+  },
+  {
+    "revision": "27561f0cfa514f9dafe4a7fdb340b2b2",
+    "url": "/magemonkeys/static/media/inner-banner.27561f0c.jpg"
+  },
+  {
+    "revision": "d672fc935f98e6386a683353bdc89636",
+    "url": "/magemonkeys/static/media/map.d672fc93.png"
+  },
+  {
+    "revision": "c4531cc451b694d072be07c7eb321aa1",
+    "url": "/magemonkeys/static/media/monkey1.c4531cc4.png"
+  },
+  {
+    "revision": "2e911648206d608eef23e746da1ac73d",
+    "url": "/magemonkeys/static/media/t-logo3.2e911648.png"
+  },
+  {
+    "revision": "164de2101e6e255087d999140f20e71e",
+    "url": "/magemonkeys/static/media/way.164de210.jpg"
+  }
+]);
